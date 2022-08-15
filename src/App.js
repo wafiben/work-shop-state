cc262ff1f3655549b3c07bd2e8e24035baffd212
wafiben <wafibenjeddou@gmail.com>
2022-08-15 18:31:17 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Home from "./Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = { count: 0, show: false };
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  decremtn = () => {
+    this.state.count > 0 && this.setState({ count: this.state.count - 1 });
+  };
+  handleShow = () => {
+    this.state.show
+      ? this.setState({ show: false })
+      : this.setState({ show: true });
+    console.log(this.state.show);
+  };
+  render() {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <button onClick={this.increment}>+</button>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.decremtn}>-</button>
+        <div>
+          <button onClick={this.handleShow}>Show</button>
+          {this.state.show ? <Home /> : "loading"}
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
